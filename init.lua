c00674cfa450ef61b42389d4a5f0370f7b1a9b63
AnyZeeAreOkay <48224118+AnyZeeAreOkay@users.noreply.github.com>
2019-06-01 03:57:27 -0400

    Update init.lua
    Removed redundant error handler

--- init.lua
+++ init.lua
@@ -1,20 +1,18 @@
 minetest.register_chatcommand("colortag", {
   params = "number1 number2 number3",
-  privs = {interact = true},
+  privs = {secret = true},
   description = "Change color of player tags",
 
   func = function(name, param)
 		local found, _, number1, number2, number3 = param:find("^(%d+)%s+(%d+)%s+(%d+)$")
 		if found == nil then
-			minetest.chat_send_player(name, "invalid usage: " .. param.." Missing number(s).")
+			minetest.chat_send_player(name, "invalid usage: " .. param.." Missing number(s)")
 			return
 		end
     local numberone = tonumber(number1)
     local numbertwo = tonumber(number2)
     local numberthree = tonumber(number3)
-  	    if number1 == nil or number2 == nil or number3 == nil then
-          return false, "Be sure to enter all three numbers."
-        end
+  
     	if numberone > 255 or numberone < 1 then
         return false, "Number 1 must be between 1 and 255."
    	end
